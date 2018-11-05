1580f77f2c876e2bc6538fcce88d50edc06c64c8
Dmitriy Dubson <d.dubson@gmail.com>
2018-11-05 09:56:29 -0500

    Able to write initial data to mongodb asynchronously - first pass

--- reactive-data/src/main/kotlin/reactive/data/ReactiveDataApp.kt
+++ reactive-data/src/main/kotlin/reactive/data/ReactiveDataApp.kt
@@ -1,6 +1,5 @@
 package reactive.data
 
-import org.reactivestreams.Publisher
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -16,19 +15,42 @@ class ReactiveDataApp
 
 fun main(args: Array<String>) {
     runApplication<ReactiveDataApp>(*args)
+
+    Thread.sleep(1000 * 10)
 }
 
 @Component
-class DataInitializer(val reservationRepository: ReservationRepository): ApplicationRunner {
+class DataInitializer(val reservationRepository: ReservationRepository) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
-        val names: Publisher<String>
+        val names: Flux<String> = Flux.just("Pete", "Julie", "Josh", "Todd")
+        val reservationFlux = names.map {
+            Reservation(null, it)
+        }
+        val map: Flux<Reservation> = reservationFlux.flatMap {
+            this.reservationRepository.save(it)
+        }
+
+        // Subscribe to the publisher (flux) -- at which point the saves will be invoked
+        map.doOnSubscribe { subscription ->
+            println("Subscribed!")
+            subscription.request(10)
+        }
+        map.doOnError {
+            println("errored. $it")
+        }
+        map.doOnComplete {
+            println("Done!")
+        }
+        map.subscribe {
+            println("new reservation. $it")
+        }
     }
 }
 
 @Document
-class Reservation(@Id val id: String,
-                  val reservationName: String)
+data class Reservation(@Id val id: String?,
+                       val reservationName: String)
 
-interface ReservationRepository: ReactiveMongoRepository<Reservation, String> {
+interface ReservationRepository : ReactiveMongoRepository<Reservation, String> {
     fun findByReservationName(rn: String): Flux<Reservation>
 }
